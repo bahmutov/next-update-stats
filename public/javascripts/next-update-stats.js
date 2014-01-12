72c3a3bc45b8a5ba06fef20e744d8fc4dd8d125b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-01-12 18:24:54 -0500

    displaying not found message, fixes #13

--- public/javascripts/next-update-stats.js
+++ public/javascripts/next-update-stats.js
@@ -10,8 +10,56 @@
   app.controller('next-update-stats-controller', function ($http, $scope) {
     $scope.packageName = 'lodash';
 
+    function processUpdates(packageName, data) {
+      verify.unemptyString(packageName, 'missing package name');
+      verify.object(data, 'undefined data for ' + packageName);
+      verify.array(data.data, 'expected array of updates for package ' + packageName);
+      $scope.packageName = packageName;
+
+      var updates = data.data.map(function (update) {
+        update.success = +update.success || 0;
+        update.failure = +update.failure || 0;
+        var total = update.success + update.failure;
+        update.probability = total === 0 ? 0 : update.success / total * 100;
+        return update;
+      });
+      $scope.updates = updates;
+
+      var fromVersions = _.groupBy(updates, 'from');
+      Object.keys(fromVersions).forEach(function (from) {
+        var to = fromVersions[from];
+        verify.array(to, 'missing info from version ' + from);
+        to = _.sortBy(to, 'to');
+        fromVersions[from] = to;
+      });
+
+      var toVersions = _(updates).groupBy('to').keys().sortBy().value();
+      verify.array(toVersions, 'expected to versions to be an array, got ' +
+        JSON.stringify(toVersions, null, 2));
+
+      $scope.header = [null].concat(toVersions);
+      $scope.info = [];
+      Object.keys(fromVersions).sort().forEach(function (from) {
+        var row = [];
+        row.length = toVersions.length + 1;
+        row[0] = from;
+        $scope.info.push(row);
+        var updates = fromVersions[from];
+        updates.forEach(function (update) {
+          var i = _.indexOf(toVersions, update.to);
+          if (i >= 0) {
+            if (row[i + 1]) {
+              throw new Error('duplicate updates for ' + $scope.packageName);
+            }
+            row[i + 1] = update;
+          }
+        });
+      });
+    }
+
     $scope.loadStats = function () {
       $scope.updates = [];
+      $scope.packageNotFoundMessage = null;
       verify.unemptyString($scope.packageName, 'expected package name string');
       if ($scope.packageName.length > 20) {
         throw new Error('invalid package name string ' + $scope.packageName);
@@ -19,47 +67,11 @@
 
       $http.get('/package/' + $scope.packageName)
       .then(function (data) {
-        verify.array(data.data, 'expected array of updates for package ' + $scope.packageName);
-
-        var updates = data.data.map(function (update) {
-          update.success = +update.success || 0;
-          update.failure = +update.failure || 0;
-          var total = update.success + update.failure;
-          update.probability = total === 0 ? 0 : update.success / total * 100;
-          return update;
-        });
-        $scope.updates = updates;
-
-        var fromVersions = _.groupBy(updates, 'from');
-        Object.keys(fromVersions).forEach(function (from) {
-          var to = fromVersions[from];
-          verify.array(to, 'missing info from version ' + from);
-          to = _.sortBy(to, 'to');
-          fromVersions[from] = to;
-        });
-
-        var toVersions = _(updates).groupBy('to').keys().sortBy().value();
-        verify.array(toVersions, 'expected to versions to be an array, got ' +
-          JSON.stringify(toVersions, null, 2));
-
-        $scope.header = [null].concat(toVersions);
-        $scope.info = [];
-        Object.keys(fromVersions).sort().forEach(function (from) {
-          var row = [];
-          row.length = toVersions.length + 1;
-          row[0] = from;
-          $scope.info.push(row);
-          var updates = fromVersions[from];
-          updates.forEach(function (update) {
-            var i = _.indexOf(toVersions, update.to);
-            if (i >= 0) {
-              if (row[i + 1]) {
-                throw new Error('duplicate updates for ' + $scope.packageName);
-              }
-              row[i + 1] = update;
-            }
-          });
-        });
+        if (!data.data || !data.data.length) {
+          $scope.packageNotFoundMessage = "Cannot find info for package " + $scope.packageName;
+        } else {
+          processUpdates($scope.packageName, data);
+        }
       });
     };
 
