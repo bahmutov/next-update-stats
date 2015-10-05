1fa7e2e967e4892b457edda981da0f1a0ef5e0e2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-05 16:56:34 -0400

    upgraded to express 4

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var routes = require('./routes');
 var initDB = require('./db/init');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var http = require('http');
 var path = require('path');
@@ -13,13 +17,13 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.configure('development', function () { app.locals.pretty = true; });
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
@@ -29,6 +33,10 @@ app.get('/version', function (req, res) {
 
 var username = process.env.STATS_DB_USERNAME;
 var password = process.env.STATS_DB_PASSWORD;
+if (!username || !password) {
+  console.error('missing db username or password');
+  process.exit(-1);
+}
 initDB(username, password).then(startServer, function (err) {
   console.error(err.stack);
 });
